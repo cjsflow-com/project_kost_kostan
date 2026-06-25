10ef3b7c0ec4e959aba31620a7e5d75bf9791137
candrajulius <candrajulius1@gmail.com>
2026-06-25 10:56:01 +0700

    fix bug error 500 because typo import

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authentic
-use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
 
 class User extends Authenticatable implements FilamentUser
