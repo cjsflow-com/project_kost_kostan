db2927dcb1c2cabd4f891f2e252162f027ed5e3d
candrajulius <candrajulius1@gmail.com>
2026-06-25 10:33:11 +0700

    fix: bug error can access panel in filament user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authentic
+use Filament\Panel;
 use Filament\Models\Contracts\FilamentUser;
 
 class User extends Authenticatable implements FilamentUser
@@ -52,4 +52,9 @@ class User extends Authenticatable implements FilamentUser
     // {
     //     return $this->hasMany(Reservation::class, 'user_id');
     // }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
